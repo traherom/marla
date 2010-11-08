d020b807385db75aa3b4d7d73feea39e788b316e
traherom <traherom@44f8cc59-6ba3-d173-c752-75036a5775c4>
2010-11-08 18:28:07 +0000

    Run scripts for GUI and JRI

--- trunk/src/r/RInterface.java
+++ trunk/src/r/RInterface.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package r;
 
 /**
@@ -19,44 +18,77 @@ import org.rosuda.JRI.RMainLoopCallbacks;
 
 class RInterface implements RMainLoopCallbacks
 {
-    public void rWriteConsole(Rengine re, String text, int oType) {
-        //do nothing
-    }
 
-    public void rBusy(Rengine re, int which) {
-        //do nothing
-    }
+	public void rWriteConsole(Rengine re, String text, int oType)
+	{
+		//do nothing
+	}
 
-    public String rReadConsole(Rengine re, String prompt, int addToHistory) {
-        try {
-            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-            String s=br.readLine();
-            return (s==null||s.length()==0)?s:s+"\n";
-        } catch (Exception e) {
-            //System.out.println("jriReadConsole exception: "+e.getMessage());
-        }
-        return null;
-    }
+	public void rBusy(Rengine re, int which)
+	{
+		//do nothing
+	}
 
-    public void rShowMessage(Rengine re, String message) {
-        //System.out.println("rShowMessage \""+message+"\"");
-    }
+	public String rReadConsole(Rengine re, String prompt, int addToHistory)
+	{
+		try
+		{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String s = br.readLine();
+			return (s == null || s.length() == 0) ? s : s + "\n";
+		}
+		catch(Exception e)
+		{
+			//System.out.println("jriReadConsole exception: "+e.getMessage());
+		}
+		return null;
+	}
 
-    public String rChooseFile(Rengine re, int newFile) {
-	FileDialog fd = new FileDialog(new Frame(), (newFile==0)?"Select a file":"Select a new file", (newFile==0)?FileDialog.LOAD:FileDialog.SAVE);
-	fd.setVisible (true);
-	String res=null;
-	if (fd.getDirectory()!=null) res=fd.getDirectory();
-	if (fd.getFile()!=null) res=(res==null)?fd.getFile():(res+fd.getFile());
-	return res;
-    }
+	public void rShowMessage(Rengine re, String message)
+	{
+		//System.out.println("rShowMessage \""+message+"\"");
+	}
 
-    public void   rFlushConsole (Rengine re) {
-    }
+	public String rChooseFile(Rengine re, int newFile)
+	{
+		FileDialog fd = new FileDialog(new Frame(), (newFile == 0) ? "Select a file" : "Select a new file", (newFile == 0) ? FileDialog.LOAD : FileDialog.SAVE);
+		fd.setVisible(true);
+		String res = null;
+		if(fd.getDirectory() != null)
+			res = fd.getDirectory();
+		if(fd.getFile() != null)
+			res = (res == null) ? fd.getFile() : (res + fd.getFile());
+		return res;
+	}
 
-    public void   rLoadHistory  (Rengine re, String filename) {
-    }
+	public void rFlushConsole(Rengine re)
+	{
+	}
 
-    public void   rSaveHistory  (Rengine re, String filename) {
-    }
+	public void rLoadHistory(Rengine re, String filename)
+	{
+	}
+
+	public void rSaveHistory(Rengine re, String filename)
+	{
+	}
+
+	/**
+	 * Check the R/JRI installation on the system to ensure R can be used
+	 * on the backend.
+	 * @return true if R could be reached to perform calculations, false otherwise
+	 */
+	public static boolean checkRInstall()
+	{
+		Rengine rengine = new Rengine(null, false, new RInterface());
+		return true;
+	}
+
+	public static void main(String[] args)
+	{
+		if(RInterface.checkRInstall())
+			System.out.println("R and JRI setup correctly");
+		else
+			System.out.println("Environment does not have R/JRI setup correctly");
+	}
 }
