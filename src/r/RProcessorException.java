f08ee05bfb7dcc673e45d1151001da30512c9901
Ryan Morehart <ryan@moreharts.com>
2010-12-09 06:54:07 +0000

    Beginnings of the new R processor. Only need to add the actual processing of returns to an ArrayList

---
+++ src/r/RProcessorException.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package r;
+
+/**
+ *
+ * @author traherom
+ */
+public class RProcessorException extends Exception {
+
+    /**
+     * Creates a new instance of <code>RProcessorException</code> without detail message.
+     */
+    public RProcessorException() {
+    }
+
+
+    /**
+     * Constructs an instance of <code>RProcessorException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public RProcessorException(String msg) {
+        super(msg);
+    }
+}
