e59e01efc14e2301b3807ec74a9f001c136e4e6c
alexdlaird <alexdlaird@44f8cc59-6ba3-d173-c752-75036a5775c4>
2011-02-22 15:50:46 +0000

    With the new implemtnation of the New Problem Wizard, data was never updated in the object.  Now it is.

--- trunk/maRlaIDE/src/gui/MainFrame.java
+++ trunk/maRlaIDE/src/gui/MainFrame.java
@@ -593,9 +593,7 @@ public class MainFrame extends JFrame
 	}//GEN-LAST:event_closeProblemMenuItemActionPerformed
 
 	private void editProblemMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editProblemMenuItemActionPerformed
-		viewPanel.NEW_PROBLEM_WIZARD_DIALOG.setTitle ("Edit Problem");
-		viewPanel.NEW_PROBLEM_WIZARD_DIALOG.welcomeTextLabel.setText (ViewPanel.welcomeEditText);
-		viewPanel.NEW_PROBLEM_WIZARD_DIALOG.launchNewProblemWizard (true);
+		viewPanel.editProblem ();
 	}//GEN-LAST:event_editProblemMenuItemActionPerformed
 
 	private void newDataSetMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_newDataSetMenuItemActionPerformed
