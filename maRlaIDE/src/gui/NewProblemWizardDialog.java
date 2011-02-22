c949b4a5efac9a818bb1e9f3e3cf665980889b41
Alex Laird <contact@alexlaird.com>
2011-02-22 20:25:01 +0000

    Fixed import issues.

--- maRlaIDE/src/gui/NewProblemWizardDialog.java
+++ maRlaIDE/src/gui/NewProblemWizardDialog.java
@@ -26,8 +26,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
--- maRlaIDE/src/gui/ToolbarButton.java
+++ maRlaIDE/src/gui/ToolbarButton.java
@@ -30,27 +30,27 @@ import javax.swing.JLabel;
  */
 public class ToolbarButton extends JLabel
 {
-    /** The default image icon.*/
-    private ImageIcon standard;
-    /** The hovered image icon.*/
-    private ImageIcon hover;
-    /** The depressed image icon.*/
-    private ImageIcon depressed;
-    /** The selected state of the button.*/
-    private boolean isSelected = false;
+	/** The default image icon.*/
+	private ImageIcon standard;
+	/** The hovered image icon.*/
+	private ImageIcon hover;
+	/** The depressed image icon.*/
+	private ImageIcon depressed;
+	/** The selected state of the button.*/
+	private boolean isSelected = false;
 
-    /**
-     * Constructs a new toolbar button with the give image icon as the default
-     * image.
-     *
-     * @param imageIcon The default image icon.
-     */
-    public ToolbarButton(ImageIcon imageIcon)
-    {
-        super ();
-        setIcon (imageIcon);
-        setIconStandards (imageIcon);
-    }
+	/**
+	 * Constructs a new toolbar button with the give image icon as the default
+	 * image.
+	 *
+	 * @param imageIcon The default image icon.
+	 */
+	public ToolbarButton(ImageIcon imageIcon)
+	{
+		super ();
+		setIcon (imageIcon);
+		setIconStandards (imageIcon);
+	}
 
 	/**
 	 * Set the standard image icons for the button.
@@ -60,62 +60,62 @@ public class ToolbarButton extends JLabel
 	public final void setIconStandards(ImageIcon imageIcon)
 	{
 		standard = imageIcon;
-        hover = new ImageIcon (getClass ().getResource ("/images/" + imageIcon.toString ().substring (imageIcon.toString ().lastIndexOf ("/") + 1, imageIcon.toString ().lastIndexOf (".")) + "_hover.png"));
-        depressed = new ImageIcon (getClass ().getResource ("/images/" + imageIcon.toString ().substring (imageIcon.toString ().lastIndexOf ("/") + 1, imageIcon.toString ().lastIndexOf (".")) + "_depressed.png"));
+		hover = new ImageIcon (getClass ().getResource ("/images/" + imageIcon.toString ().substring (imageIcon.toString ().lastIndexOf ("/") + 1, imageIcon.toString ().lastIndexOf (".")) + "_hover.png"));
+		depressed = new ImageIcon (getClass ().getResource ("/images/" + imageIcon.toString ().substring (imageIcon.toString ().lastIndexOf ("/") + 1, imageIcon.toString ().lastIndexOf (".")) + "_depressed.png"));
 	}
 
-    /**
-     * Sets the selected state of the button.
-     * 
-     * @param state True to set depressed, false otherwise.
-     */
-    public void setSelected(boolean state)
-    {
-        isSelected = state;
-        setDepressed (state);
-    }
+	/**
+	 * Sets the selected state of the button.
+	 *
+	 * @param state True to set depressed, false otherwise.
+	 */
+	public void setSelected(boolean state)
+	{
+		isSelected = state;
+		setDepressed (state);
+	}
 
-    /**
-     * Returns the selected state of the button.
-     *
-     * @return The selected state of the button.
-     */
-    public boolean isSelected()
-    {
-        return isSelected;
-    }
+	/**
+	 * Returns the selected state of the button.
+	 *
+	 * @return The selected state of the button.
+	 */
+	public boolean isSelected()
+	{
+		return isSelected;
+	}
 
-    /**
-     * Sets the hover state of the button.
-     *
-     * @param state True to set hover, false otherwise.
-     */
-    public void setHover(boolean state)
-    {
-        if (state)
-        {
-            setIcon (hover);
-        }
-        else
-        {
-            setIcon (standard);
-        }
-    }
+	/**
+	 * Sets the hover state of the button.
+	 *
+	 * @param state True to set hover, false otherwise.
+	 */
+	public void setHover(boolean state)
+	{
+		if (state)
+		{
+			setIcon (hover);
+		}
+		else
+		{
+			setIcon (standard);
+		}
+	}
 
-    /**
-     * Sets the depressed state of the button.
-     *
-     * @param state True to set depressed, false otherwise.
-     */
-    public void setDepressed(boolean state)
-    {
-        if (state)
-        {
-            setIcon (depressed);
-        }
-        else
-        {
-            setIcon (standard);
-        }
-    }
+	/**
+	 * Sets the depressed state of the button.
+	 *
+	 * @param state True to set depressed, false otherwise.
+	 */
+	public void setDepressed(boolean state)
+	{
+		if (state)
+		{
+			setIcon (depressed);
+		}
+		else
+		{
+			setIcon (standard);
+		}
+	}
 }
