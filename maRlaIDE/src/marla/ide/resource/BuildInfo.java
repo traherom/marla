9680dd682ac1b99fb7a9e533f1e5e7a72330603b
Alex Laird <contact@alexlaird.com>
2011-03-24 20:07:44 +0000

    Fixed issue I created in the NSIS script.

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="416";
-				public static final String timeStamp="Thu, 24-March-2011 15:51:53 EDT";
+				public static final String revisionNumber="417";
+				public static final String timeStamp="Thu, 24-March-2011 16:06:44 EDT";
 			}
 		
