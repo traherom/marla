1f6dc0d1f431c035f3281f979f255f6c60919e83
Ryan Morehart <ryan@moreharts.com>
2011-03-24 16:26:42 +0000

    - Packaging requires a clean

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="409";
-				public static final String timeStamp="Thu, 24-March-2011 12:21:13 EDT";
+				public static final String revisionNumber="411";
+				public static final String timeStamp="Thu, 24-March-2011 12:25:10 EDT";
 			}
 		
