8486e1aa300a78aef6ee0714b8dd6db8bfaaaae4
Alex Laird <contact@alexlaird.com>
2011-03-24 21:55:04 +0000

    Fixed shortuct icon issues.

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="417";
-				public static final String timeStamp="Thu, 24-March-2011 16:06:44 EDT";
+				public static final String revisionNumber="420";
+				public static final String timeStamp="Thu, 24-March-2011 17:54:05 EDT";
 			}
 		
