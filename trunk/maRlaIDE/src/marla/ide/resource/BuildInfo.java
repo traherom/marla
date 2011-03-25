2ac0648ca109bb8ab4cda01822a0cd62191ca3a9
alexdlaird <alexdlaird@44f8cc59-6ba3-d173-c752-75036a5775c4>
2011-03-25 02:35:57 +0000

    Updated the installer to have a finalized page.

--- trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="424";
-				public static final String timeStamp="Thu, 24-March-2011 22:26:11 EDT";
+				public static final String revisionNumber="423";
+				public static final String timeStamp="Thu, 24-March-2011 22:32:50 EDT";
 			}
 		
