d3718dd24c243679894337190e3e61eba1d511d8
Ryan Morehart <ryan@moreharts.com>
2011-03-25 04:02:49 +0000

    Correctly escaped ^ in latex export

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="423";
-				public static final String timeStamp="Thu, 24-March-2011 22:32:50 EDT";
+				public static final String revisionNumber="429";
+				public static final String timeStamp="Fri, 25-March-2011 00:00:13 EDT";
 			}
 		
