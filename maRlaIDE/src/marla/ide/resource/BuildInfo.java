6d6f71b2dfafe7bc5dbe4748a5ab1255cfaacca7
Ryan Morehart <ryan@moreharts.com>
2011-03-29 13:42:29 +0000

    Added flushlog() to marlaide

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="474";
-				public static final String timeStamp="Mon, 28-March-2011 22:29:38 EDT";
+				public static final String revisionNumber="475";
+				public static final String timeStamp="Tue, 29-March-2011 09:42:02 EDT";
 			}
 		
