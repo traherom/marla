46646a00d7add88c7295db63a05498f59166d867
morehart <morehart@44f8cc59-6ba3-d173-c752-75036a5775c4>
2011-04-01 18:15:23 +0000

    Added getVisibleData() to Problem

--- trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="536";
-				public static final String timeStamp="Fri, 1-April-2011 14:03:56 EDT";
+				public static final String revisionNumber="538";
+				public static final String timeStamp="Fri, 1-April-2011 14:10:20 EDT";
 			}
 		
