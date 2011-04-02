afa09b1dcef4946c0f585324e79fb6c81b616fa9
morehart <morehart@44f8cc59-6ba3-d173-c752-75036a5775c4>
2011-04-02 19:46:19 +0000

    Added meta-build.xml that allows javadocto be easily produced for both projects and the installer to be created

--- trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="560";
-				public static final String timeStamp="Sat, 2-April-2011 14:57:18 EDT";
+				public static final String revisionNumber="561";
+				public static final String timeStamp="Sat, 2-April-2011 15:35:34 EDT";
 			}
 		
