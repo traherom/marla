d3549a0ca75ed711b14a30ab659cc11d709caaf3
Ryan Morehart <ryan@moreharts.com>
2011-04-06 14:06:26 +0000

    Answers/names now get saved properly to copied operations

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="605";
-				public static final String timeStamp="Wed, 6-April-2011 01:39:49 EDT";
+				public static final String revisionNumber="606";
+				public static final String timeStamp="Wed, 6-April-2011 10:03:31 EDT";
 			}
 		
