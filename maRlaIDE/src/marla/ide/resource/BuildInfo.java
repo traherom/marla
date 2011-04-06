82ecfa7403d1755ef304c547ae49adb229d2fada
Ryan Morehart <ryan@moreharts.com>
2011-04-06 16:58:26 +0000

    No longer tell the domain when we're changing

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="607";
-				public static final String timeStamp="Wed, 6-April-2011 10:49:47 EDT";
+				public static final String revisionNumber="608";
+				public static final String timeStamp="Wed, 6-April-2011 12:42:18 EDT";
 			}
 		
