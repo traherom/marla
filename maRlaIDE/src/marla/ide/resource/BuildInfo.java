96317716f57c1503ea00ea2b50c1bda3145f9bdc
Ryan Morehart <ryan@moreharts.com>
2011-04-11 18:49:33 +0000

    Include OS and user name in exception reports

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="633";
-				public static final String timeStamp="Fri, 8-April-2011 14:32:17 EDT";
+				public static final String revisionNumber="636";
+				public static final String timeStamp="Mon, 11-April-2011 14:43:29 EDT";
 			}
 		
