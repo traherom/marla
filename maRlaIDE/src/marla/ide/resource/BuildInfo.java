d4eabc3b8661e35f74224953aefa6387be81d292
Ryan Morehart <ryan@moreharts.com>
2011-04-13 12:42:48 +0000

    Made installer deselect shortcuts if maRla is not going to be installed

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="640";
-				public static final String timeStamp="Wed, 13-April-2011 08:31:54 EDT";
+				public static final String revisionNumber="641";
+				public static final String timeStamp="Wed, 13-April-2011 08:41:23 EDT";
 			}
 		
