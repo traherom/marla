739ce6a2914bc9cd3c860e17c34d1fd4942408eb
Ryan Morehart <ryan@moreharts.com>
2011-04-14 15:53:57 +0000

    - Made operation prompts stay in order of insertion (IE, as they are in the XML). Should have slight speed benefits - Added two checks on XML to avoid null pointers

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="655";
-				public static final String timeStamp="Thu, 14-April-2011 10:05:21 EDT";
+				public static final String revisionNumber="658";
+				public static final String timeStamp="Thu, 14-April-2011 11:46:11 EDT";
 			}
 		
