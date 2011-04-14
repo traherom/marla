03feb826435954af035b0fe6696fba2afd1dc422
Ryan Morehart <ryan@moreharts.com>
2011-04-14 18:31:10 +0000

    Fixed some tree building issues. Much cleaner code now, IMO

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="659";
-				public static final String timeStamp="Thu, 14-April-2011 12:27:27 EDT";
+				public static final String revisionNumber="660";
+				public static final String timeStamp="Thu, 14-April-2011 14:30:20 EDT";
 			}
 		
