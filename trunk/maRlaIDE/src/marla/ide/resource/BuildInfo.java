d21e9e139ebdcc1cfd509ffaa445f1e5110bce23
morehart <morehart@44f8cc59-6ba3-d173-c752-75036a5775c4>
2011-04-14 00:23:45 +0000

    - Fixed a few tiny installer issues/strings - Updated version number

--- trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="649";
-				public static final String timeStamp="Wed, 13-April-2011 20:04:10 EDT";
+				public static final String revisionNumber="650";
+				public static final String timeStamp="Wed, 13-April-2011 20:18:59 EDT";
 			}
 		
