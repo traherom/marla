5a029559024a50aaa074d93ee93e1fa1f33684ec
Ryan Morehart <ryan@moreharts.com>
2011-04-15 01:52:39 +0000

    - Made installer check on (re)install that maRla isn't already running. - Made failures of extra software no kill rest of installation

--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="662";
-				public static final String timeStamp="Thu, 14-April-2011 14:49:27 EDT";
+				public static final String revisionNumber="664";
+				public static final String timeStamp="Thu, 14-April-2011 21:46:05 EDT";
 			}
 		
