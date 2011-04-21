6c3cb88e05e78dc3621e558d39992f29883d8a65
alexdlaird <alexdlaird@44f8cc59-6ba3-d173-c752-75036a5775c4>
2011-04-21 19:08:07 +0000

    images in Op Editor now point to IDE domain rel path variable. Can't edit parameters (or view R code) of an unattached operation now (it caused an exception).

--- trunk/maRlaOperationEditor/src/marla/opedit/gui/MainFrame.java
+++ trunk/maRlaOperationEditor/src/marla/opedit/gui/MainFrame.java
@@ -160,7 +160,7 @@ public class MainFrame extends JFrame
         redoMenuItem = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
-        setIconImage(new ImageIcon (getClass ().getResource ("/marla/ide/images/logo.png")).getImage ());
+        setIconImage(new ImageIcon (getClass ().getResource (marla.ide.gui.Domain.IMAGES_DIR + "logo.png")).getImage ());
         getContentPane().setLayout(new java.awt.GridLayout(1, 1));
 
         editMenuItem.setText("Edit");
