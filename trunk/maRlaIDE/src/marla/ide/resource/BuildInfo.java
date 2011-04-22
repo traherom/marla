f4f94f52086f250cf6ff83f274888ab7c42b41c9
morehart <morehart@44f8cc59-6ba3-d173-c752-75036a5775c4>
2011-04-22 22:42:45 +0000

    Added rest of two-sample tests

--- trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="699";
-				public static final String timeStamp="Fri, 22-April-2011 14:18:43 EDT";
+				public static final String revisionNumber="700";
+				public static final String timeStamp="Fri, 22-April-2011 18:39:26 EDT";
 			}
 		
