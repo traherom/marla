e77979fc504fd93873524855ab04af9688a730f8
Ryan Morehart <ryan@moreharts.com>
2011-04-23 16:09:25 +0000

    Fixed various errors in the upload script

--- maRlaIDE/PushVersion.py
+++ maRlaIDE/PushVersion.py
@@ -25,33 +25,36 @@ def googleCode(verNum, verPre, windowsSetupFile, zipFile):
     username = raw_input("\nUsername: ")
     password = getpass.getpass()
     try:
-        print ("\n::UPLOADS BEGINNING FOR v" + verNum + verPre + "::")
+        print ("\n::UPLOADS BEGINNING FOR v" + str(verNum) + verPre + "::")
         print ("::DO NOT TERMINATE UNTIL COMPLETE::")
         totalSuccess = True
         project = "marla"
         
         print ("Uploading Windows installation (" + str(round(float(os.path.getsize(windowsSetupFile)) / float(1048576), 1)) + "MB)...")
-        code = GoogleCodeUpload.upload(windowsSetupFile, project, username, password, "The maRla Project " + verNum + verPre + " for Windows", "The Windows installer for The maRla Project, a statistics IDE developed for students to help them work through and solve complicated statistical calculations. The maRla Project requires the R statistical package as well as LaTeX to run, though this Windows installer will automatically download, install, and configure these programs for you if you do not already have them. The maRla Project requires the Java Runtime Environment (JRE) to run, though this installer will download and install the JRE for you if you do not already have it.", ["Featured", "OpSys-Windows", "Type-Installer"])[0]
+        code = GoogleCodeUpload.upload(windowsSetupFile, project, username, password, "The maRla Project " + str(verNum) + verPre + " for Windows", "The Windows installer for The maRla Project, a statistics IDE developed for students to help them work through and solve complicated statistical calculations. The maRla Project requires the R statistical package as well as LaTeX to run, though this Windows installer will automatically download, install, and configure these programs for you if you do not already have them. The maRla Project requires the Java Runtime Environment (JRE) to run, though this installer will download and install the JRE for you if you do not already have it.", ["Featured", "OpSys-Windows", "Type-Installer"])[0]
         if code != 201:
             totalSuccess = False
-            print ("The upload may not have succeeded, error code " + code + ".  Either there was a miscommunication with the server, or the given filename already exists on Google Code.")
+            print ("The upload may not have succeeded, error code " + str(code) + ".  Either there was a miscommunication with the server, or the given filename already exists on Google Code.")
                 
         print ("Uploading cross-platform archive (" + str(round(float(os.path.getsize(zipFile)) / float(1048576), 1)) + "MB)...")
-        code = GoogleCodeUpload.upload(zipFile, project, username, password, "The maRla Project " + verNum + verPre + " for any operating system", "The cross-platform version of The maRla Project, a statistics IDE developed for students to help them work through and solve complicated statistical calculations. The maRla Project requires the R statistical package as well as LaTeX to run. This version will run on Windows, Mac, Linux, and any other operation system that supports Java.", ["Featured", "OpSys-All", "Type-Archive"])
+        code = GoogleCodeUpload.upload(zipFile, project, username, password, "The maRla Project " + str(verNum) + verPre + " for any operating system", "The cross-platform version of The maRla Project, a statistics IDE developed for students to help them work through and solve complicated statistical calculations. The maRla Project requires the R statistical package as well as LaTeX to run. This version will run on Windows, Mac, Linux, and any other operation system that supports Java.", ["Featured", "OpSys-All", "Type-Archive"])[0]
         if code != 201:
             totalSuccess = False
-            print ("The upload may not have succeeded, error code " + code + ".  Either there was a miscommunication with the server, or the given filename already exists on Google Code.")
+            print ("The upload may not have succeeded, error code " + str(code) + ".  Either there was a miscommunication with the server, or the given filename already exists on Google Code.")
         
         if totalSuccess:
             print ("::UPLOADS COMPLETE::")
         
-            print ("The maRla Project was successfully updated to v" + verNum + verPre + " on Google Code.")
+            print ("The maRla Project was successfully updated to v" + str(verNum) + verPre + " on Google Code.")
             print ("You will still need to update the revision file on the Wiki so user's are informed of this update.")
         else:
             print ("::UPLOADS ATTEMPTED::")
             
             print ("One or more of the uploads encountered problems. Verify that all files are up to date on the Google Code Downloads page.")
-    except:
+    except Exception as e:
+    	print type(e)
+    	print e.args
+    	print e
         print ("An unknown error occured. Try running the push again. If this error persists, it is recommended that you ensure the validity of all version files and installers on Google Code immediately.")
 
 ## Calls respective helper methods to complete overall task of ensuring the
@@ -74,13 +77,13 @@ def main(args):
         if os.path.exists(windowsSetupFile) and os.path.exists(zipFile):
             print ("::ALL NECESSARY FILES IN PLACE::")
                         
-            answer = raw_input ("\nWould you like to update the files and installers on Google Code? ")
+            answer = raw_input ("\nWould you like to update the files and installers on Google Code (y/n)? ")
             if "yes" in answer.lower() or answer.lower() == "y":
                 googleCode(verNum, verPre, windowsSetupFile, zipFile)
                 
             print ("")
     else:
-        print ("The directory structure on your system does not match that required by this script.\nEnsure Get Organized has been built to the latest version and all portable and installation files are in place.")
+        print ("The directory structure on your system does not match that required by this script.\nEnsure The maRla Project has been built to the latest version and all installation files are in place.")
         print ("")
     
 ## This is the entry point for the program, which figures off the main method to
--- maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="722";
-				public static final String timeStamp="Sat, 23-April-2011 08:49:21 EDT";
+				public static final String revisionNumber="741";
+				public static final String timeStamp="Sat, 23-April-2011 11:42:29 EDT";
 			}
 		
