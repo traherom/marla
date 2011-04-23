90e6e9f9bbd73a12750211bae871740bd554dd82
alexdlaird <alexdlaird@44f8cc59-6ba3-d173-c752-75036a5775c4>
2011-04-23 16:55:57 +0000

    Updated push to state that it doesn't build. Added help button in op editor.

--- trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
+++ trunk/maRlaIDE/src/marla/ide/resource/BuildInfo.java
@@ -5,7 +5,7 @@
 			 */
 			package marla.ide.resource;
 			public class BuildInfo {
-				public static final String revisionNumber="741";
-				public static final String timeStamp="Sat, 23-April-2011 11:42:29 EDT";
+				public static final String revisionNumber="743";
+				public static final String timeStamp="Sat, 23-April-2011 12:48:47 EDT";
 			}
 		
